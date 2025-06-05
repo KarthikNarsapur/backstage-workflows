48c1857bdc1d83925ffa5b5b2a7707a75a036a06
sumit-ct-02 <sumit.d@cloudthat.com>
2025-06-05 10:22:16 +0530

    Update main.tf

--- terraform/ebs/delete_ebs/main.tf
+++ terraform/ebs/delete_ebs/main.tf
@@ -9,7 +9,7 @@ terraform {
 
 terraform {
   backend "s3" {
-    bucket = "interns-backstage.io-statefile"
+    bucket = "backstage-interns-project"
     key    = "project/ebs.tfstate"
     region = "ap-south-1"
     encrypt = true
