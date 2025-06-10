ac8cd9d18d2f75f63ea0465d2f0bf1dedfd54927
Karthik N <karthik.n@cloudthat.com>
2025-06-10 15:51:20 +0530

    Update main.tf

--- terraform/ec2/create_ec2/main.tf
+++ terraform/ec2/create_ec2/main.tf
@@ -53,3 +53,11 @@ resource "aws_instance" "ec2_instance" {
 }
 
 
+output "instance_id" {
+  value = aws_instance.ec2_instance.id
+}
+output "public_ip" {
+  value = aws_instance.ec2_instance.public_ip
+}
+ 
+
